37871b2497c611c0252204f4f470ee66f70f406f
mads962j <s223870@student.dtu.dk>
2025-03-11 17:34:00 +0100

    Working graph of ibl growth and friction velocity

--- IBL_visualization.py
+++ IBL_visualization.py
@@ -3,13 +3,13 @@ from matplotlib.legend_handler import HandlerTuple
 import numpy as np
 import math as m
 
-delta_ibl_0 = 100
-z_0hi = 0.92
-z_h = 100
+delta_ibl_0 = 70
+z_0hi = 0.68
+z_h = 70
 s_x = 7
-D = 100
-x_len = 10000
-x = np.arange(0, x_len)
+D = 80
+x_len = 10000/z_0hi
+x = np.arange(0, x_len, x_len)
 H_G  = 1300
 
 def ibl_visualization(delta_ibl_0, z_0hi, z_h, s, D, x_len, H_G):
@@ -19,6 +19,7 @@ def ibl_visualization(delta_ibl_0, z_0hi, z_h, s, D, x_len, H_G):
     ibl = ibl(x)
     ibl[ibl >= H_G] = H_G  # Limit IBL height
 
+    
     # Define turbine positions
     p_bottom = [(i * s * D, 0) for i in range(0, m.ceil(x_len / (s * D)))]
     p_top = [(i * s * D, z_h) for i in range(0, m.ceil(x_len / (s * D)))]
@@ -32,7 +33,8 @@ def ibl_visualization(delta_ibl_0, z_0hi, z_h, s, D, x_len, H_G):
     for (bx, by), (tx, ty) in zip(p_bottom, p_top):
         line, = ax.plot([bx, tx], [by, ty], color='Blue', linestyle='-')  
         turbine_plots.append(line)
-
+    
+    
     # Plot IBL height and store its handle
     ibl_plot, = ax.plot(x, ibl, label='IBL height [m]', color="Green")
 
@@ -53,4 +55,4 @@ def ibl_visualization(delta_ibl_0, z_0hi, z_h, s, D, x_len, H_G):
     plt.grid(True)
     plt.show()
 
-#ibl_visualization(delta_ibl_0, z_0hi, z_h, s, D, x_len, H_G)
+ibl_visualization(delta_ibl_0, z_0hi, z_h, s_x, D, x_len, H_G)
